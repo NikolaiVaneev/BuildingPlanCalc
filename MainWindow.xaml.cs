966338d417ec9319fddb3ddc1a67f1d7967586d2
Nikolay <nikolai_vaneev@mail.ru>
2021-11-23 22:40:50 +0300

    Пофикшено сворачивание приложения при развернутом состоянии

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using BuildingPlanCalc.Interfaces;
 using BuildingPlanCalc.Models;
 using BuildingPlanCalc.Services;
+using BuildingPlanCalc.UI;
 using System;
 using System.Collections.Generic;
 using System.Globalization;
@@ -26,6 +27,8 @@ namespace BuildingPlanCalc
     {
         public MainWindow()
         {
+            MaxHeight = SystemParameters.MaximizedPrimaryScreenHeight;
+            MaxWidth = SystemParameters.MaximizedPrimaryScreenWidth;
             InitializeComponent();
             if (!CheckAppLicense())
             {
@@ -49,7 +52,7 @@ namespace BuildingPlanCalc
                 new KeyGesture(Key.N, ModifierKeys.Control)));
 
             CommandBindings.Add(new CommandBinding(ApplicationCommands.Delete,
-                (sender, e) => { ClearAllMainLine(); },
+                (sender, e) => { ClearSelectedLayout(); },
                 (sender, e) => { e.CanExecute = true; }));
 
             InputBindings.Add(new KeyBinding(ApplicationCommands.Delete,
@@ -71,7 +74,7 @@ namespace BuildingPlanCalc
 
         // Переменные для выравнивания линии по осям
         private bool axisAligment;
-        private byte sector;
+
         byte ShapeType { get; set; } = 1;
         byte SelectedBuidingObj = 0;
 
@@ -87,46 +90,7 @@ namespace BuildingPlanCalc
         }
 
         private bool mRestoreIfMove = false;
-        private void Window_SourceInitialized(object sender, EventArgs e)
-        {
-            IntPtr mWindowHandle = new WindowInteropHelper(this).Handle;
-            HwndSource.FromHwnd(mWindowHandle).AddHook(new HwndSourceHook(WindowProc));
-        }
-        private static IntPtr WindowProc(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
-        {
-            switch (msg)
-            {
-                case 0x0024:
-                    WmGetMinMaxInfo(hwnd, lParam);
-                    break;
-            }
 
-            return IntPtr.Zero;
-        }
-        private static void WmGetMinMaxInfo(IntPtr hwnd, IntPtr lParam)
-        {
-            POINT lMousePosition;
-            GetCursorPos(out lMousePosition);
-
-            IntPtr lCurrentScreen = MonitorFromPoint(lMousePosition, MonitorOptions.MONITOR_DEFAULTTONEAREST);
-
-
-            MINMAXINFO lMmi = (MINMAXINFO)Marshal.PtrToStructure(lParam, typeof(MINMAXINFO));
-
-            MONITORINFO lCurrentScreenInfo = new MONITORINFO();
-            if (GetMonitorInfo(lCurrentScreen, lCurrentScreenInfo) == false)
-            {
-                return;
-            }
-
-            //Position relative pour notre fenêtre
-            lMmi.ptMaxPosition.X = lCurrentScreenInfo.rcWork.Left - lCurrentScreenInfo.rcMonitor.Left;
-            lMmi.ptMaxPosition.Y = lCurrentScreenInfo.rcWork.Top - lCurrentScreenInfo.rcMonitor.Top;
-            lMmi.ptMaxSize.X = lCurrentScreenInfo.rcWork.Right - lCurrentScreenInfo.rcWork.Left;
-            lMmi.ptMaxSize.Y = lCurrentScreenInfo.rcWork.Bottom - lCurrentScreenInfo.rcWork.Top;
-
-            Marshal.StructureToPtr(lMmi, lParam, true);
-        }
         private void SwitchWindowState()
         {
             switch (WindowState)
@@ -134,72 +98,18 @@ namespace BuildingPlanCalc
                 case WindowState.Normal:
                     {
                         WindowState = WindowState.Maximized;
+                        TControl.Height = window.ActualHeight - 280;
                         break;
                     }
                 case WindowState.Maximized:
                     {
                         WindowState = WindowState.Normal;
+                        TControl.Height = window.ActualHeight - 275;
                         break;
                     }
             }
         }
-        [DllImport("user32.dll")]
-        [return: MarshalAs(UnmanagedType.Bool)]
-        static extern bool GetCursorPos(out POINT lpPoint);
 
-        [DllImport("user32.dll", SetLastError = true)]
-        static extern IntPtr MonitorFromPoint(POINT pt, MonitorOptions dwFlags);
-        private enum MonitorOptions : uint
-        {
-            MONITOR_DEFAULTTONULL = 0x00000000,
-            MONITOR_DEFAULTTOPRIMARY = 0x00000001,
-            MONITOR_DEFAULTTONEAREST = 0x00000002
-        }
-
-        [DllImport("user32.dll")]
-        static extern bool GetMonitorInfo(IntPtr hMonitor, MONITORINFO lpmi);
-        [StructLayout(LayoutKind.Sequential)]
-        public struct POINT
-        {
-            public int X;
-            public int Y;
-
-            public POINT(int x, int y)
-            {
-                this.X = x;
-                this.Y = y;
-            }
-        }
-        [StructLayout(LayoutKind.Sequential)]
-        public struct MINMAXINFO
-        {
-            public POINT ptReserved;
-            public POINT ptMaxSize;
-            public POINT ptMaxPosition;
-            public POINT ptMinTrackSize;
-            public POINT ptMaxTrackSize;
-        };
-
-        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Auto)]
-        public class MONITORINFO
-        {
-            public int cbSize = Marshal.SizeOf(typeof(MONITORINFO));
-            public RECT rcMonitor = new RECT();
-            public RECT rcWork = new RECT();
-            public int dwFlags = 0;
-        }
-        [StructLayout(LayoutKind.Sequential)]
-        public struct RECT
-        {
-            public int Left, Top, Right, Bottom;
-            public RECT(int left, int top, int right, int bottom)
-            {
-                Left = left;
-                Top = top;
-                Right = right;
-                Bottom = bottom;
-            }
-        }
 
         private void Header_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
@@ -230,20 +140,6 @@ namespace BuildingPlanCalc
             if (mRestoreIfMove)
             {
                 mRestoreIfMove = false;
-
-                double percentHorizontal = e.GetPosition(this).X / ActualWidth;
-                double targetHorizontal = RestoreBounds.Width * percentHorizontal;
-
-                double percentVertical = e.GetPosition(this).Y / ActualHeight;
-                double targetVertical = RestoreBounds.Height * percentVertical;
-
-                WindowState = WindowState.Normal;
-
-                GetCursorPos(out POINT lMousePosition);
-
-                Left = lMousePosition.X - targetHorizontal;
-                Top = lMousePosition.Y - targetVertical;
-
                 DragMove();
             }
         }
@@ -253,7 +149,7 @@ namespace BuildingPlanCalc
         }
         private void window_SizeChanged(object sender, SizeChangedEventArgs e)
         {
-            TControl.Height = window.ActualHeight - 270;
+            TControl.Height = window.ActualHeight - 275;
         }
         #endregion
 
@@ -3158,6 +3054,13 @@ namespace BuildingPlanCalc
                 axisAligment = false;
             }
         }
+
+        private void window_Loaded(object sender, RoutedEventArgs e)
+        {
+            var transparencyConverter = new TransparencyConverter(this);
+            transparencyConverter.MakeTransparent();
+        }
+
 
 
     }
